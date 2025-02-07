eefcd8a39f7a67e7d51e03191915df1b71757936
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-02-07 16:26:13 +0100

    feat:api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\TollController;
+use App\Http\Controllers\Api\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post("/tolls", [TollController::class, "store"])->name("apitollstore");
+Route::post("/vehicles", [VehicleController::class, "store"])->name("apivehiclestore");
+Route::put("/vehicles/{id}/tolls/{tollId}", [VehicleController::class, "goThroughToll"])->name("apiattachtoll");
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\TollController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view("welcome");
 });
+
+Route::get('/tolls', [TollController::class, "index"])->name("tolls");
+Route::get('/vehicles', [VehicleController::class, "index"])->name("vehicles");
