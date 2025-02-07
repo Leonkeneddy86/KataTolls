3bf3af4ec872dadc718b93046e13d63d3713ba32
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-02-07 16:10:44 +0100

    feat: database & seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,57 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Toll;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Vehicle;
+use App\Models\VehicleType;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+        
+        Toll::create([
+            "name" => "toll1",
+            "city" => "Malaga",
+            "earned" => 0
+        ]);
+
+        VehicleType::create([
+            "type" => "car",
+            "price" => 100
+        ]);
+
+        VehicleType::create([
+            "type" => "motorbike",
+            "price" => 50
+        ]);
+
+        VehicleType::create([
+            "type" => "2 axle truck",
+            "price" => 100
+        ]);
+
+        VehicleType::create([
+            "type" => "3 axle truck",
+            "price" => 150
+        ]);
+
+        VehicleType::create([
+            "type" => "4 axle truck",
+            "price" => 200
+        ]);
+
+        $vehicle = Vehicle::create([
+            "vehicle_type_id" => 1,
+            "registration" => "X1296C2",
+            "spent" => 0
         ]);
     }
 }
